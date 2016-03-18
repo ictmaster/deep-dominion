62db6c6aefe04e925c71167226f601b77ae2d74e
Jonas Natten <jonasn12@student.uia.no>
2016-03-18 12:06:00 +0100

    ADDS: Multiprocessing scraper of log links

--- jlib.py
+++ jlib.py
@@ -1,6 +1,7 @@
 from datetime import datetime
-def LOG(string):
-	print(string)
+def LOG(string, print_log=True):
+	if print_log:
+		print(string)
 	logfile = "./logfile.log"
 	with open(logfile, 'a') as logfile:
 		logfile.write("["+str(datetime.now())+"]>> "+string+'\n')
--- scraper.py
+++ scraper.py
@@ -4,16 +4,59 @@ from pyquery import PyQuery as pq
 from lxml import etree
 import urllib
 from os.path import basename, isfile
+from os import remove
 from jlib import LOG
+from multiprocessing import Pool, cpu_count
+import sys
+global done_users
+global user_data
 
-download_folder = './data/goko/'
+def get_loglinks(user_tuple):
+    username = user_tuple[0]
+    games = user_tuple[1]
+    LOG("Fetching for {games} games with user {username}".format(games=games, username=username))
+    links = []
+    for offset in range(0, games, 1000):
+        logsearch = pq(url='http://gokosalvager.com/logsearch?p1name={username}&p1score=any&p2name=&startdate=08%2F05%2F2012&enddate=03%2F17%2F2016&supply=&nonsupply=&rating=professional&pcount=2&colony=any&bot=false&shelters=any&guest=false&minturns=&maxturns=&quit=false&resign=any&limit=1000&submitted=true&offset={offset}'.format(offset=offset, username=username))    
+        for link in logsearch('a:contains("Log")')[1:]:
+            links.append(link)
+    LOG("Fetched links for user {username}".format(username=username))
+    return links
 
-LOG("Fetching leaderboard...")
-leaderboard = pq(url='http://gokosalvager.com/leaderboard/')
-leaderboard_rows = leaderboard('table:first tr')
-usernames = []
-total_size = 0
-all_links = []
+def download_textfile(url):
+    pass # TODO: MAKE THIS
+
+if __name__ == '__main__':
+    download_folder = './data/goko/'
+    linkfile = 'linkfile.txt'
+    if 'refetch' in sys.argv or not isfile(linkfile):
+        LOG("Fetching leaderboard...")
+        leaderboard = pq(url='http://gokosalvager.com/leaderboard/')
+        leaderboard_rows = leaderboard('table:first tr')
+        user_data = []
+
+        if isfile(linkfile):
+            remove(linkfile)
+
+        LOG("Fetching logs...")
+        for row_index, row in enumerate(leaderboard_rows):
+            try:
+                user_data.append((row[-1].text, int(row[-2].text)))
+            except ValueError:
+                LOG("ERROR when parsing user_data... (index: {0})".format(row_index))
+                continue
+
+        pool = Pool(processes=cpu_count())            
+        for res in pool.map(get_loglinks, [u for u in user_data]):
+            with open(linkfile,'a') as lf:
+                for l in res:
+                    lf.write(l+'\n')
+        pool.close()
+        pool.join()
+
+        # TODO: remove duplicates from linkfile
+
+"""
 for row_index, row in enumerate(leaderboard_rows):
     username = row[-1].text
     try:
@@ -28,14 +71,13 @@ for row_index, row in enumerate(leaderboard_rows):
                 fname = download_folder+basename(urllib.parse.urlparse(link).path)
                 if not isfile(fname):
                     try:
-                        urllib.request.urlretrieve(link, fname)
+                        pass # urllib.request.urlretrieve(link, fname)
                     except urllib.error.HTTPError as httpe:
                         LOG("{code} error given by {fname} ({link})".format(code=httpe.code, fname=fname, link=link))
                         continue
                 status = (r"{index}/{size} [{per}] [TOTAL:{tper}]".format(index=index,size=len(log_links), per=("%3.2f%%" % (float(index)*100/len(log_links))), tper=("%3.2f%%" % (float(row_index)*100/len(leaderboard_rows)))))
                 print (status, end="\r")
-            total_size += len(log_links)
-            all_links.extend(log_links)
     except ValueError:
         continue
     LOG("DOWNLOADED FINISHED FOR {user} now at {index}/{size} [{per}]".format(user=username, index=row_index, size=len(leaderboard_rows), per="%3.2f%%" % (float(row_index)*100/len(leaderboard_rows))))
+"""
