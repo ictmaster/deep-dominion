b32d169e570ed288592faa763d557a962a9aca38
Jonas Natten <jonasn12@student.uia.no>
2016-03-18 09:27:56 +0100

    ADDS: Error handling in scraper.py

--- scraper.py
+++ scraper.py
@@ -16,10 +16,10 @@ total_size = 0
 all_links = []
 for row_index, row in enumerate(leaderboard_rows):
     username = row[-1].text
-    log_links = []
     try:
         for offset in range(0, int(row[-2].text), 1000):
             logsearch = pq(url='http://gokosalvager.com/logsearch?p1name={username}&p1score=any&p2name=&startdate=08%2F05%2F2012&enddate=03%2F17%2F2016&supply=&nonsupply=&rating=professional&pcount=2&colony=any&bot=false&shelters=any&guest=false&minturns=&maxturns=&quit=false&resign=any&limit=1000&submitted=true&offset={offset}'.format(offset=offset, username=username))    
+            log_links = []
             for log in logsearch('a:contains("Log")')[1:]:
                 log_links.append(pq(log).attr.href)
 
@@ -27,12 +27,15 @@ for row_index, row in enumerate(leaderboard_rows):
             for index, link in enumerate(log_links):
                 fname = download_folder+basename(urllib.parse.urlparse(link).path)
                 if not isfile(fname):
-                    urllib.request.urlretrieve(link, fname)
+                    try:
+                        urllib.request.urlretrieve(link, fname)
+                    except urllib.error.HTTPError as httpe:
+                        LOG("{code} error given by {fname} ({link})".format(code=httpe.code, fname=fname, link=link))
+                        continue
                 status = (r"{index}/{size} [{per}] [TOTAL:{tper}]".format(index=index,size=len(log_links), per=("%3.2f%%" % (float(index)*100/len(log_links))), tper=("%3.2f%%" % (float(row_index)*100/len(leaderboard_rows)))))
                 print (status, end="\r")
-        total_size += len(log_links)
-        print(total_size)
-        all_links.extend(log_links)
+            total_size += len(log_links)
+            all_links.extend(log_links)
     except ValueError:
         continue
-    LOG("DOWNLOADED FINISHED FOR {user}".format(user=username))
+    LOG("DOWNLOADED FINISHED FOR {user} now at {index}/{size} [{per}]".format(user=username, index=row_index, size=len(leaderboard_rows), per="%3.2f%%" % (float(row_index)*100/len(leaderboard_rows))))
