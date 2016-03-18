daeee5aa8ec0282dcf4a37c296128ac273dbf07c
Jonas Natten <jonasn12@student.uia.no>
2016-03-18 16:13:46 +0100

    FIXES: Multiprocessing of links. (Writing to file)

--- scraper.py
+++ scraper.py
@@ -7,26 +7,36 @@ from os.path import basename, isfile
 from os import remove
 from jlib import LOG
 from multiprocessing import Pool, cpu_count
+from threading import Thread
 import sys
-global done_users
-global user_data
+import time
 
 def get_loglinks(user_tuple):
     username = user_tuple[0]
     games = user_tuple[1]
-    LOG("Fetching for {games} games with user {username}".format(games=games, username=username))
+    LOG("Fetching for {games} games with user {username}".format(games=games, username=username), False)
     links = []
     for offset in range(0, games, 1000):
         logsearch = pq(url='http://gokosalvager.com/logsearch?p1name={username}&p1score=any&p2name=&startdate=08%2F05%2F2012&enddate=03%2F17%2F2016&supply=&nonsupply=&rating=professional&pcount=2&colony=any&bot=false&shelters=any&guest=false&minturns=&maxturns=&quit=false&resign=any&limit=1000&submitted=true&offset={offset}'.format(offset=offset, username=username))    
         for link in logsearch('a:contains("Log")')[1:]:
-            links.append(link)
-    LOG("Fetched links for user {username}".format(username=username))
+            links.append(pq(link).attr.href)
+    LOG("Fetched links for user {username}".format(username=username), False)
     return links
 
 def download_textfile(url):
-    pass # TODO: MAKE THIS
+    with open(linkfile,'r') as lf:
+        for line in lf:
+            
+
+def print_progress(size, start_time):
+    global done
+    while True:
+        status = (r"{done}/{size} [{per}] - Elapsed time: {seconds} seconds".format(done=done,size=size, per=("%3.2f%%" % (float(done)*100/size)), seconds=("%d") % (time.time()-start_time)))
+        print(status, end='\r')
+        time.sleep(1)
 
 if __name__ == '__main__':
+    start_time = time.time()
     download_folder = './data/goko/'
     linkfile = 'linkfile.txt'
     if 'refetch' in sys.argv or not isfile(linkfile):
@@ -39,20 +49,28 @@ if __name__ == '__main__':
             remove(linkfile)
 
         LOG("Fetching logs...")
-        for row_index, row in enumerate(leaderboard_rows):
+        for row_index, row in enumerate(leaderboard_rows)[1:]:
             try:
                 user_data.append((row[-1].text, int(row[-2].text)))
             except ValueError:
                 LOG("ERROR when parsing user_data... (index: {0})".format(row_index))
                 continue
 
+        global done
+        done = 0
+
+        print_thread = Thread(target=print_progress, args=[len(user_data), start_time])
+        print_thread.start()
         pool = Pool(processes=cpu_count())            
-        for res in pool.map(get_loglinks, [u for u in user_data]):
-            with open(linkfile,'a') as lf:
+        with open(linkfile,'a') as lf:
+            for res in pool.imap_unordered(get_loglinks, user_data):
                 for l in res:
                     lf.write(l+'\n')
+                done += 1
         pool.close()
         pool.join()
+
+        print_thread.join()
 
         # TODO: remove duplicates from linkfile
 
