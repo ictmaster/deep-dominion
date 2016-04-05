81d3605cdf8bc46fee6def98b5239614d86e4b2d
Jonas Natten <jonasn12@student.uia.no>
2016-04-05 20:42:53 +0200

    Begins implementing parsing script

--- parse.py
+++ parse.py
@@ -1,36 +1,52 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 from jlib import LOG
-from lxml import html, etree
-from pyquery import PyQuery as pq
 import os
-import re
 import time
+from pymongo import MongoClient
+import re
 
-# Using scandir since folder is so large
+
 start_time = time.time()
 
-# Regular expression for finding turn stuff
-turn_regex = re.compile('\s*--- .*\'s turn (\d) ---$')
+client = MongoClient()
+db = client.dominion
 
-for entry in os.scandir('./data/min_data_extracted/'):
-	with open(entry.path, 'r', encoding='cp850') as html_file:
-		html_str = html_file.read()
-		try:
-			q = pq(html.fromstring(html_str))
-		except etree.XMLSyntaxError:
-			print(entry.path)
-		stripped = q.text()
-		game = {}
+
+turn_regex = re.compile('---------- \w+: turn \d+ ----------')
+
+# Using scandir since folder is so large
+for entry in os.scandir('./data/min_goko/'):
+	with open(entry.path, 'r', encoding='cp850') as f:
+		data = f.read()
+		document = {}
+		supply_match = "Supply cards: "
+		starting_match = "- starting cards: "
+		starting_cards = {}
+		players = {}
 		current_turn = -1
-		for line in stripped.splitlines():
-			if re.match(turn_regex, line):
-				current_turn = int(line.split()[-2])
-			if 'cards in supply:' in line:
-				game['cards'] = line.replace('cards in supply:','').replace(',','').replace('and','').split()
+		current_player = -1
 
-			if current_turn != -1:
-				game['turns'] = game.get('turns', {})
+		for line in data.split('\n'):
+			# Game setup
+			if line.find(supply_match) != -1:
+				supply_cards = list(map(str.strip, line[line.find(supply_match)+len(supply_match):].split(',')))
+			if line.find(starting_match) != -1:
+				cards = line[line.find(starting_match)+len(starting_match):].split(',')
+				player = line[:line.find(starting_match)].strip()
+				players[player] = len(players)
+				starting_cards[players[player]] = cards
+			# Who's turn and turn nr
+			if turn_regex.match(line):
+				current_player = players[line[11:].split(':')[0]]
+				current_turn = line.split('turn ')[1][:-10]
+			
+		# supply_cards = list(map(str.strip, next(line[line.find(supply_match)+len(supply_match):] for line in data.split('\n') if line.find(supply_match) != -1).split(',')))
+		
+		#print(document)
+		# result = db.logs.insert_one()
+		
 
 
-print("Script took {0} seconds...".format(time.time()-start_time))
+
+print("Script took {0:0.4f} seconds...".format(time.time()-start_time))
